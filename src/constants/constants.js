e3406114362029b4df72a4a7181fd1c156c840b0
robert morales <rmoralfit@gmail.com>
2022-05-02 20:12:22 -0400

    refactoring components.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'React-Shopping-Cart',
     description: "This project I created a little web shop with a shopping cart. As the consumer you can add items, delete items, and displays the total sum amount.",
     image: '/images/6.png',
-    tags: ['ReactQuery', 'MaterialUI', 'TypeScript', 'StyleComponents'],
+    tags: ['ReactQuery', 'MaterialUI', 'TypeScript'],
     source: 'https://google.com',
     visit: 'https://github.com/rmorales723/react-shopping-cart',
     id: 3,
